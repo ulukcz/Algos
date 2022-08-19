e5a63954fd129e24ada3d453a43066910a1a9a4f
ulukcz <achykkg@gmail.com>
2022-08-19 20:51:43 +0200

    IfDuplicates doing with sort

--- src/TaskOne.java
+++ src/TaskOne.java
@@ -6,7 +6,7 @@ public class TaskOne {
         System.out.println(Arrays.toString(targetSumHashMap(new int[]{3, 8, 15, 17}, 23)));
         System.out.println(Arrays.toString(targetSumBruteForce(new int[]{3, 4}, 7)));
         // Task #2
-        System.out.println(ifDuplicatesList(new int[]{4, 5, 6, 6, 8}));
+        System.out.println(ifDuplicatesSort(new int[]{4, 5, 6, 6, 8}));
         System.out.println(ifDuplicatesBruteForce(new int[]{4, 5, 6, 7, 8}));
     }
     //HashMap O(n)
@@ -34,14 +34,15 @@ public class TaskOne {
         return null;
     }
 
-    // ArrayList O(n)
-    public static boolean ifDuplicatesList(int[] array) {
-        List<Integer> list = new ArrayList<>();
-        for (int num : array) {
-            if (list.contains(num)) {
+    // ArrayList O(nlogn)
+    public static boolean ifDuplicatesSort(int[] array) {
+        Arrays.sort(array);
+        int num = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (num == array[i])
                 return true;
-            }
-            list.add(num);
+            else
+                num = array[i];
         }
         return false;
     }
